57f832545d18f09d5f73ea709b6a41e5b55e3d98
Biraj Prajapati <biraj.prajapati@workday.com>
2021-07-09 13:23:19 +0100

    testing script for token

--- scripts/git_api_curl.sh
+++ scripts/git_api_curl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-echo ${GITHUB_SECURITY_TOKEN:0:1}
+echo $TEST_VARIABLE | head -c 1
 
 curl -X POST -H "Accept: application/json" -H "Authorization: Bearer $GITHUB_SECURITY_TOKEN" https://api.github.com/repos/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commits/$BUILDKITE_COMMIT/comments -d '{"body":"Testing tagging @peakon/security in comment"}'
 
