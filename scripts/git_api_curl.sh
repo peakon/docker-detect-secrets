50afa3e77d7ef6854f161a1e19c594ed0b46aa7c
Biraj Prajapati <biraj.prajapati@workday.com>
2021-07-09 13:17:07 +0100

    testing script for token

--- scripts/git_api_curl.sh
+++ scripts/git_api_curl.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [[ -z "${GITHUB_SECURITY_TOKEN}" ]]; then
-    echo "Github Sec Token Not Set"
-else
-    echo "SEC Token SET"
-    curl -X POST -H "Accept: application/json" -H "Authorization: Bearer ${GITHUB_SECURITY_TOKEN}" https://api.github.com/repos/${BUILDKITE_ORGANIZATION_SLUG}/${BUILDKITE_PIPELINE_SLUG}/commits/${BUILDKITE_COMMIT}/comments -d '{"body":"Testing tagging @peakon/security in comment"}'
-fi
+
+
+echo ${GITHUB_SECURITY_TOKEN:0:1}
+
+curl -X POST -H "Accept: application/json" -H "Authorization: Bearer $GITHUB_SECURITY_TOKEN" https://api.github.com/repos/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commits/$BUILDKITE_COMMIT/comments -d '{"body":"Testing tagging @peakon/security in comment"}'
+
 
