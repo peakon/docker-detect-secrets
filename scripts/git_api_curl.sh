837ac06a35274bea6ec448020df8008a81dcbae8
Biraj Prajapati <biraj.prajapati@workday.com>
2021-07-12 12:17:28 +0100

    more tests for env variable

--- scripts/git_api_curl.sh
+++ scripts/git_api_curl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-echo https://api.github.com/repos/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commits/$BUILDKITE_COMMIT/comments
+echo https://api.github.com/repos/${BUILDKITE_ORGANIZATION_SLUG}/${BUILDKITE_PIPELINE_SLUG}/commits/${BUILDKITE_COMMIT}/comments
 
 
 curl -X POST -H "Accept: application/json" -H "Authorization: Bearer $GITHUB_SECURITY_ACCESS_TOKEN" https://api.github.com/repos/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commits/$BUILDKITE_COMMIT/comments -d '{"body":"Testing tagging @peakon/security in comment"}'
