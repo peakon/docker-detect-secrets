104403d5cb173709a3d23de37575417ebd8adbc1
Biraj Prajapati <biraj.prajapati@workday.com>
2021-07-15 19:05:10 +0100

    add script to fine tune detect-secret

---
+++ scripts/pre-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+detect-secrets scan --disable-plugin ArtifactoryDetector --disable-plugin CloudantDetector --disable-plugin IbmCloudIamDetector --disable-plugin IbmCosHmacDetector --disable-plugin SoftlayerDetector --disable-plugin StripeDetector --disable-plugin TwilioKeyDetector
+
+detect-secrets scan --exclude-files '.*\.*spec.js$' --exclude-files '.*fixtures.*' --exclude-files '.*\/translations\/locales\/.*.json$'
