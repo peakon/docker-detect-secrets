37eb834c733701f8c690c30152fb138e6d5d0d84
Biraj Prajapati <biraj.prajapati@workday.com>
2021-07-28 21:11:15 +0545

    change curl configuration

--- scripts/git_api_curl.sh
+++ scripts/git_api_curl.sh
@@ -2,6 +2,5 @@
 
 curl -X POST -H "Accept: application/json" -H "Authorization: Bearer $GITHUB_SECURITY_ACCESS_TOKEN" https://api.github.com/repos/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commits/$BUILDKITE_COMMIT/comments -d '{"body":"A new potential secret was discovered in this commit @peakon/security"}'
 COMMIT_URL=https://github.com/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commits/$BUILDKITE_COMMIT
-ATTACHMENT={"color": "warning", "title": "A new potential secret was commited in Github", "text": "$COMMIT_URL"}
-curl -X POST -H 'Content-type: application/json' --data {"text":"Potential secret in Github!", "attachments":"[$ATTACHMENT]"} $SF_SECURITY_SLACK_WEBHOOK_URL
+curl -X POST -H 'Content-type: application/json' --data {"text":"Potential secret in Github!", "attachments":"[{'color': 'warning', 'title': 'A new potential secret was commited in Github', 'text': '$COMMIT_URL'}]"} $SF_SECURITY_SLACK_WEBHOOK_URL
 
