3e10ed3d6041e825a459c525e4128f67a2d2d6f9
Biraj Prajapati <biraj.prajapati@workday.com>
2021-07-28 21:31:32 +0545

    change curl configuration

--- scripts/git_api_curl.sh
+++ scripts/git_api_curl.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 curl -X POST -H "Accept: application/json" -H "Authorization: Bearer $GITHUB_SECURITY_ACCESS_TOKEN" https://api.github.com/repos/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commits/$BUILDKITE_COMMIT/comments -d '{"body":"A new potential secret was discovered in this commit @peakon/security"}'
-COMMIT_URL=https://github.com/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commits/$BUILDKITE_COMMIT
-curl -X POST -H 'Content-type: application/json' --data {"text":"Potential secret in Github!", "attachments":"[{'color': 'warning', 'title': 'A new potential secret was commited in Github', 'text': '$COMMIT_URL'}]"} $SF_SECURITY_SLACK_WEBHOOK_URL
+curl -X POST -H 'Content-type: application/json' --data {"text":"Potential secret in Github!", "attachments":"[{'color': 'warning', 'title': 'A new potential secret was commited in Github', 'text': 'https://github.com/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commits/$BUILDKITE_COMMIT'}]"} $SF_SECURITY_SLACK_WEBHOOK_URL
 
