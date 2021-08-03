b795bd10e55d2a197a348f00d281024c48d94401
Biraj Prajapati <biraj.prajapati@workday.com>
2021-08-03 18:59:34 +0545

    echo curl response

--- scripts/git_api_curl.sh
+++ scripts/git_api_curl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 curl -X POST -H "Accept: application/json" -H "Authorization: Bearer $GITHUB_SECURITY_ACCESS_TOKEN" https://api.github.com/repos/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commits/$BUILDKITE_COMMIT/comments -d '{"body":"A new potential secret was discovered in this commit @peakon/security"}'
-SLACK_HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -H 'Content-type: application/json' --data "{\"text\":\"Potential secret in Github!\", \"attachments\":[{\"color\": \"warning\", \"title\": \"A new potential secret was commited in Github\", \"text\": \"https://github.com/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commit/$BUILDKITE_COMMIT\"}]}" $SF_DEFAULT_SLACK_WEBHOOK_URL)
-echo $SLACK_HTTP_RESPONSE
+SLACK_HTTP_RESPONSE=$(curl --silent -X POST -H 'Content-type: application/json' --data "{\"text\":\"Potential secret in Github!\", \"attachments\":[{\"color\": \"warning\", \"title\": \"A new potential secret was commited in Github\", \"text\": \"https://github.com/$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG/commit/$BUILDKITE_COMMIT\"}]}" $SF_DEFAULT_SLACK_WEBHOOK_URL)
+echo "Slack Response: $SLACK_HTTP_RESPONSE"
 
